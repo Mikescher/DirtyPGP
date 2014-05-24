8734316075c324f552f415d1752f0387ed768fec
Mike Schwörer <mailport@mikescher.de>
2014-05-24 18:12:04 +0200

    Added Generate_Tab

---
+++ DirtyPGP/RASHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Numerics;
+
+namespace DirtyPGP
+{
+	public static class RASHelper
+	{
+		private static Random random = new Random();
+
+		public static bool IsPrime(long number)
+		{
+			long i;
+			for (i = 2; i <= number - 1; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}
+			}
+			if (i == number)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public static long GetPrime()
+		{
+			long start = random.Next(10000) * 2 + 1001;
+
+			while (!IsPrime(start))
+			{
+				start -= 2;
+			}
+
+			return start;
+		}
+
+		public static long GetMultInv(long _e, long _m)
+		{
+			BigInteger d = new BigInteger(1);
+
+			BigInteger e = new BigInteger(_e);
+			BigInteger m = new BigInteger(_m);
+
+			while ((e * d) % m != 1)
+			{
+				d++;
+			}
+
+			return (long)d;
+		}
+
+		public static long GGT(long a, long b)
+		{
+			long tmp = 0;
+			while (b != 0)
+			{
+				tmp = a % b;
+				a = b;
+				b = tmp;
+			}
+			return a;
+		}
+
+		public static long LongRandom(long min, long max)
+		{
+			byte[] buf = new byte[8];
+			random.NextBytes(buf);
+			long longRand = BitConverter.ToInt64(buf, 0);
+
+			return (Math.Abs(longRand % (max - min)) + min);
+		}
+
+		public static long GetRandomCoprime(long m)
+		{
+			long v = LongRandom(2, m);
+
+			while (GGT(v, m) != 1)
+			{
+				v++;
+				if (v == m)
+					v = 2;
+			}
+
+			return v;
+		}
+	}
+}
--- DirtyPGP/Tab_Generate.xaml.cs
+++ DirtyPGP/Tab_Generate.xaml.cs
@@ -1,15 +1,98 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace DirtyPGP
 {
 	/// <summary>
-	/// Interaction logic for Tab_Generate.xaml
+	/// longeraction logic for Tab_Generate.xaml
 	/// </summary>
 	public partial class Tab_Generate : UserControl
 	{
+		private bool intialized = false;
+
 		public Tab_Generate()
 		{
 			InitializeComponent();
+
+			intialized = true;
+
+			testValues();
+		}
+
+		private void ed_ValueChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<object> e)
+		{
+			testValues();
+		}
+
+		private void testValues()
+		{
+			if (!intialized)
+				return;
+
+			SolidColorBrush b_true = new SolidColorBrush(Colors.Green);
+			SolidColorBrush b_false = new SolidColorBrush(Colors.Red);
+
+			long p = ed_var_p.Value.Value;
+			long q = ed_var_q.Value.Value;
+			long n = ed_var_n.Value.Value;
+			long m = ed_var_m.Value.Value;
+			long e = ed_var_e.Value.Value;
+			long d = ed_var_d.Value.Value;
+
+			ell_test_p.Fill = RASHelper.IsPrime(p) ? b_true : b_false;
+			ell_test_q.Fill = RASHelper.IsPrime(q) ? b_true : b_false;
+
+			ell_test_n.Fill = (n == p * q) ? b_true : b_false;
+			ell_test_m.Fill = (m == (p - 1) * (q - 1)) ? b_true : b_false;
+
+			ell_test_e.Fill = RASHelper.GGT(e, m) == 1 ? b_true : b_false;
+
+			ell_test_d.Fill = (m != 0 && (e * d) % m == 1) ? b_true : b_false;
+
+			bool correct = RASHelper.IsPrime(p) && RASHelper.IsPrime(q) && (n == p * q) && (m == (p - 1) * (q - 1)) && (RASHelper.GGT(e, m) == 1) && (m != 0 && (e * d) % m == 1);
+
+			lblPublicKey.Content = correct ? string.Format("PUBLIC KEY = (e|N) = ({0}|{1})", e, n) : "ERROR";
+			lblPrivateKey.Content = correct ? string.Format("PRIVATE KEY = (d|N) = ({0}|{1})", d, n) : "ERROR";
+		}
+		private void btn_gen_p_Click(object sender, System.Windows.RoutedEventArgs eargs)
+		{
+			ed_var_p.Value = RASHelper.GetPrime();
+		}
+
+		private void btn_gen_q_Click(object sender, System.Windows.RoutedEventArgs eargs)
+		{
+			ed_var_q.Value = RASHelper.GetPrime();
+		}
+
+		private void btn_gen_n_Click(object sender, System.Windows.RoutedEventArgs eargs)
+		{
+			long p = ed_var_p.Value.Value;
+			long q = ed_var_q.Value.Value;
+
+			ed_var_n.Value = p * q;
+		}
+
+		private void btn_gen_m_Click(object sender, System.Windows.RoutedEventArgs eargs)
+		{
+			long p = ed_var_p.Value.Value;
+			long q = ed_var_q.Value.Value;
+
+			ed_var_m.Value = (p - 1) * (q - 1);
+		}
+
+		private void btn_gen_e_Click(object sender, System.Windows.RoutedEventArgs eargs)
+		{
+			long m = ed_var_m.Value.Value;
+
+			ed_var_e.Value = RASHelper.GetRandomCoprime(m);
+		}
+
+		private void btn_gen_d_Click(object sender, System.Windows.RoutedEventArgs eargs)
+		{
+			long m = ed_var_m.Value.Value;
+			long e = ed_var_e.Value.Value;
+
+			ed_var_d.Value = RASHelper.GetMultInv(e, m);
 		}
 	}
 }
